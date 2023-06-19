06ed3cede3d258fb00c19ad47338063f2f9b338b
Andrei Nepsha <nepsha.a.a@ya.ru>
2023-06-19 11:20:36 +0800

    Improve 2741 solution

--- _contest/weekly-350/M.2741.special-permutations.py
+++ _contest/weekly-350/M.2741.special-permutations.py
@@ -2,32 +2,27 @@ MOD = 10 ** 9 + 7
 
 class Solution:
     def specialPerm(self, nums: list[int]) -> int:
-        ln = len(nums)
         dp = {}
-        num_to_ind = {n: i for i, n in enumerate(nums)}
-        mask = ["0"] * ln
-        used = set()
+        num_to_mask = {n: 1 << i for i, n in enumerate(nums)}
+        all_used = sum(v for v in num_to_mask.values())
 
-        def dfs(prev: int):
-            key = f'{prev}_{"".join(mask)}'
+        def dfs(prev: int, used_mask):
+            key = prev << 16 | used_mask
             if key in dp:
                 return dp[key]
 
-            if len(used) == ln:
+            if used_mask == all_used:
                 return 1
 
             perms = 0
             for n in nums:
-                if n not in used and (prev == -1 or prev % n == 0 or n % prev == 0):
-                    used.add(n)
-                    mask[num_to_ind[n]] = "1"
-                    perms = (perms + dfs(n)) % MOD
-                    mask[num_to_ind[n]] = "0"
-                    used.remove(n)
+                m = num_to_mask[n]
+                if m & used_mask == 0 and (prev % n == 0 or n % prev == 0):
+                    perms = (perms + dfs(n, used_mask | num_to_mask[n])) % MOD
             dp[key] = perms
             return perms
 
-        return dfs(-1)
+        return dfs(1, 0)
 
 
 a = Solution().specialPerm(nums = [19,38,76,152,304,608,1216,2432,4864,9728,19456,38912,77824,155648])
